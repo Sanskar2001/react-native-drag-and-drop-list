f61ee1da17c6ac48aef0f85b0ba08a9878ded5d9
Sanskar2001 <sanskaratrey22@gmail.com>
2023-07-26 00:12:24 +0530

    removed node_modules

--- node_modules/@testing-library/react-native/src/__tests__/timerUtils.ts
+++
@@ -1,7 +0,0 @@
-import { setTimeout } from '../helpers/timers';
-
-async function sleep(ms: number): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-export { sleep };
